cd005dad884a7460bb20c57a68434cc49db60e2d
AliVik <alvikul1994@gmail.com>
2022-01-21 23:12:15 +0200

    создаёт компоненты для поиска картинок

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import css from "./Searchbar.module.css";
+
+export default class Searchbar extends Component {
+  state = {
+    formInput: "",
+  };
+  onInputChange = (evt) => {
+    this.setState({ formInput: evt.target.value });
+  };
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    this.props.onSubmit(this.state);
+    form.reset();
+  };
+  render() {
+    return (
+      <header className={css.header}>
+        <form onSubmit={this.onFormSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.onInputChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
---
+++ src/helpers/fetchImages.js
@@ -0,0 +1,9 @@
+// const API_URL =
+//   "https://pixabay.com/api/?q=cat&page=1&key=`${API_KEY}`&image_type=photo&orientation=horizontal&per_page=12";
+
+// const API = {
+//     key: "24371502-78d84e7e9c9a76cd0b2a52a11",
+//     url: `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+// };
+
+export default class QueryToApi {}
