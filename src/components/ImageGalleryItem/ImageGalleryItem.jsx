6d64f9ea6fa275fd22a187d456d6030a80d488e5
AliVik <alvikul1994@gmail.com>
2022-01-22 18:47:39 +0200

    создаёт компонент элемента списка картинок

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,11 @@
-export default function ImageGalleryItem() {
+import css from "./ImageGalleryItem.module.css";
+
+export default function ImageGalleryItem({ id, webformatURL, largeImageURL }) {
   return (
-    <li class="gallery-item">
-      <img src="" alt="" />
+    <li key={id} className={css.GalleryItem}>
+      <a href={largeImageURL}>
+        <img src={webformatURL} alt="" className={css.GalleryImage} />
+      </a>
     </li>
   );
 }
